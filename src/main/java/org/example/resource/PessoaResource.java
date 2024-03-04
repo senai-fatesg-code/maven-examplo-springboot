8da45c628d4c1c9accbe9714a8f947f0a3c2af2d
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-03-04 20:00:44 -0300

    chore: setup do projeto

--- src/main/java/org/example/resource/PessoaResource.java
+++ src/main/java/org/example/resource/PessoaResource.java
@@ -1,13 +1,12 @@
 package org.example.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/pessoa")
-public class Pessoa {
+public class PessoaResource {
 
 
     @GetMapping
